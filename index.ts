095a5b15d4e5d0f62ee0654a0eb57bc80c04e315
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-21 17:35:22 -0400

    chore: update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+import { CustomFile, Folder } from './patterns/composite/composite.class';
+import {
+  BaseNotifier,
+  EmailNotifier,
+  PushNotifier,
+  SMSNotifier,
+} from './patterns/decorator/decorator.class';
 import {
   Caretaker,
   ConcreteMemento,
@@ -7,6 +14,8 @@ import print from './utils/print';
 
 const editor = new TextEditor('Hola mundo');
 const caretaker = new Caretaker<string>();
+
+print('-------------------Memento-------------------');
 
 // Guardamos el estado actual
 caretaker.addMemento(editor.save());
@@ -34,3 +43,42 @@ editor.restore(caretaker.undo() ?? new ConcreteMemento<string>(''));
 
 print(caretaker.getMementos());
 print(editor.content); // Output: Contenido editado
+
+print('-------------------Decorator-------------------');
+
+// Ejemplo de uso
+const baseNotifier = new BaseNotifier();
+baseNotifier.send('Notificación base');
+
+const smsNotifier = new SMSNotifier(baseNotifier);
+smsNotifier.send('Notificación importante');
+
+const emailNotifier = new EmailNotifier(baseNotifier);
+emailNotifier.send('Notificación urgente');
+
+const pushNotifier = new PushNotifier(baseNotifier);
+pushNotifier.send('Notificación prioritaria');
+
+print('-------------------Composite-------------------');
+// Ejemplo de uso
+const file1 = new CustomFile('Archivo1.txt', 100); // Tamaño: 100 bytes
+const file2 = new CustomFile('Archivo2.txt', 200); // Tamaño: 200 bytes
+const file3 = new CustomFile('Archivo3.txt', 150); // Tamaño: 150 bytes
+
+const folder1 = new Folder('Carpeta 1');
+folder1.add(file1);
+folder1.add(file2);
+
+const folder2 = new Folder('Carpeta 2');
+folder2.add(file3);
+
+const rootFolder = new Folder('Carpeta raíz');
+rootFolder.add(folder1);
+rootFolder.add(folder2);
+
+print('Tamaño total de ', folder1.getName() + ': ' + String(folder1.getSize())); // Output: Tamaño total de la carpeta 1: 300
+print('Tamaño total de ', folder2.getName() + ': ' + String(folder2.getSize())); // Output: Tamaño total de la carpeta 2: 150
+print(
+  'Tamaño total de ',
+  rootFolder.getName() + ': ' + String(rootFolder.getSize())
+); // Output: Tamaño total de la carpeta raíz: 450
