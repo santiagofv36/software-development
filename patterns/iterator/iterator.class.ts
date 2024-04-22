0c83a7060fce8a57610e565049b5465f533bd74d
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-21 22:04:24 -0400

    fix: Iterator more comprehensive

--- patterns/iterator/iterator.class.ts
+++ patterns/iterator/iterator.class.ts
@@ -1,6 +1,6 @@
-class ListNode<T> {
+class MyNode<T> {
   data: T;
-  next: ListNode<T> | null;
+  next: MyNode<T> | null;
 
   constructor(data: T) {
     this.data = data;
@@ -8,45 +8,82 @@ class ListNode<T> {
   }
 }
 
-class LinkedListIterator<T> implements Iterator<T> {
-  private current: ListNode<T> | null;
-
-  constructor(head: ListNode<T> | null) {
-    this.current = head;
-  }
-
-  public next(): IteratorResult<T> {
-    if (this.current === null) {
-      return { done: true, value: undefined as any };
-    } else {
-      const value = this.current.data;
-      this.current = this.current.next;
-      return { done: false, value };
-    }
-  }
-}
-
-export class LinkedList<T> implements Iterable<T> {
-  private head: ListNode<T> | null;
+export class LinkedList<T> {
+  private head: MyNode<T> | null;
 
   constructor() {
     this.head = null;
   }
 
-  public append(data: T): void {
-    if (this.head === null) {
-      this.head = new ListNode(data);
+  add(data: T): void {
+    const newMyNode = new MyNode<T>(data);
+    if (!this.head) {
+      this.head = newMyNode;
     } else {
       let current = this.head;
-      while (current.next !== null) {
+      while (current.next) {
         current = current.next;
       }
-      current.next = new ListNode(data);
+      current.next = newMyNode;
     }
   }
 
-  [Symbol.iterator](): Iterator<T> {
-    return new LinkedListIterator(this.head);
+  // delete the last element
+
+  pop(): void {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next!.next) {
+      current = current.next!;
+    }
+    current.next = null;
+  }
+
+  // Custom iterable factory method
+  createIterable(): CustomIterable<T> {
+    return new CustomIterable(this.head);
   }
 }
 
+class CustomIterable<T> {
+  private head: MyNode<T> | null;
+
+  constructor(head: MyNode<T> | null) {
+    this.head = head;
+  }
+
+  // Custom iterator factory method
+  createIterator(): CustomIterator<T> {
+    return new CustomIterator(this.head);
+  }
+}
+
+class CustomIterator<T> {
+  private current: MyNode<T> | null;
+
+  constructor(head: MyNode<T> | null) {
+    this.current = head;
+  }
+
+  hasNext(): boolean {
+    return !!this.current;
+  }
+
+  next(): T | null {
+    if (this.hasNext()) {
+      const { data } = this.current!;
+      this.current = this.current!.next;
+      return data;
+    } else {
+      return null;
+    }
+  }
+}
