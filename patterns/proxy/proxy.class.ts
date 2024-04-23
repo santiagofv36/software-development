49514bdf13c6d3473289cd9413166c09a022e07c
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-23 14:41:58 -0400

    feat: Proxy Pattern added

---
+++ patterns/proxy/proxy.class.ts
@@ -0,0 +1,37 @@
+// Interfaz para el Servidor Web
+interface WebServer {
+  request(url: string): void;
+}
+
+// Implementación del Servidor Web Externo
+class RealWebServer implements WebServer {
+  request(url: string): void {
+    console.log(`Solicitud enviada al servidor externo para la URL: ${url}`);
+  }
+}
+
+// Implementación del Proxy del Servidor Web
+class WebServerProxy implements WebServer {
+  private realServer: RealWebServer;
+  private bannedSites: Set<string>;
+
+  constructor() {
+    this.realServer = new RealWebServer();
+    this.bannedSites = new Set<string>();
+    // Agregamos algunos sitios prohibidos para simular la filtración
+    this.bannedSites.add('sitio1.com');
+    this.bannedSites.add('sitio2.com');
+  }
+
+  request(url: string): void {
+    if (this.isBanned(url)) {
+      console.log(`Acceso bloqueado al sitio: ${url}`);
+    } else {
+      this.realServer.request(url);
+    }
+  }
+
+  private isBanned(url: string): boolean {
+    return this.bannedSites.has(url);
+  }
+}
