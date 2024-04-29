52bcada2af8b165e38e4e41b9b713eb26b948464
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-04-28 21:06:09 -0400

    feat: Finishing Exam1

--- excersises/exam1.ts
+++ excersises/exam1.ts
@@ -31,6 +31,18 @@ class Nodo<T> {
   cumple(p: Predicado<T>): number {
     let nodosCumplen = 0;
 
+    if (this.predicado.cumple(this.valor)) {
+      nodosCumplen++;
+    }
+
+    for (const hijo of this.hijos) {
+      nodosCumplen += hijo.cumple(p);
+    }
+
+    if (this.nodoInterno) {
+      nodosCumplen += this.nodoInterno.cumple(p);
+    }
+
     return nodosCumplen;
   }
 
@@ -45,6 +57,14 @@ class NodoCompuesto<T> extends Nodo<T> {
   cumple(p: Predicado<T>): number {
     let nodosCumplen = 0;
 
+    if (this.predicado.cumple(this.valor)) {
+      nodosCumplen++;
+    }
+
+    for (const hijo of this.hijos) {
+      nodosCumplen += hijo.cumple(p);
+    }
+
     return nodosCumplen;
   }
 
@@ -54,6 +74,30 @@ class NodoCompuesto<T> extends Nodo<T> {
 
   aplicar(f: Funcion<T>, p: Predicado<T>): void {}
 }
+
+class PredicadoNumber implements Predicado<number> {
+  cumple(t: number): boolean {
+    return t % 2 !== 0;
+  }
+}
+
+const p = new PredicadoNumber();
+
+const nodo1 = new Nodo<number>('nodo 1', 1, p);
+const nodo2 = new Nodo<number>('nodo 2', 2, p);
+const nodo3 = new Nodo<number>('nodo 3', 3, p);
+const nodo4 = new Nodo<number>('nodo 4', 4, p);
+const nodo5 = new Nodo<number>('nodo 5', 5, p);
+
+const nodoCompuesto = new NodoCompuesto<number>('Nodo compuesto', 0, p);
+
+nodoCompuesto.getHijos().push(nodo1, nodo2, nodo3);
+
+nodo2.getHijos().push(nodo4);
+nodo3.getHijos().push(nodo5);
+
+const cantidadCumple = nodoCompuesto.cumple(p);
+console.log('Cantidad que cumplen', cantidadCumple);
 
 // #endregion
 
@@ -65,11 +109,106 @@ class NodoCompuesto<T> extends Nodo<T> {
 
 // #region Pregunta 3
 
-
 // #endregion
 
 // #region Pregunta 4
 
+interface UIComponent<T> {
+  build(): void;
+
+  changeState(state: T): void;
+
+  observers: Component<T>[];
+}
+
+class Component<T> implements UIComponent<T> {
+  observers: Component<T>[] = [];
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  build(): void {
+    console.log('Renderizando pantalla de', this.name);
+  }
+
+  changeState(state: T): void {
+    console.log('Cambiando estado del componente concreto', state, this.name);
+    this.notify(state);
+  }
+  // subscribe
+  addObserver(observer: Component<T>): void {
+    this.observers.push(observer);
+  }
+  // unsubscribe
+  removeObserver(observer: Component<T>): void {
+    this.observers = this.observers.filter((o) => o !== observer);
+  }
+
+  notify(state: T): void {
+    this.observers.forEach((o) => o.changeState(state));
+  }
+
+  addChild(child: Component<T>): void {
+    throw new Error('Method not implemented');
+  }
+
+  removeChild(child: Component<T>): void {
+    throw new Error('Method not implemented');
+  }
+}
+
+class Decorator<T> implements UIComponent<T> {
+  observers: Component<T>[] = [];
+
+  constructor(private component: Component<T>) {}
+
+  build(): void {
+    this.component.build();
+    this.decorate();
+  }
+
+  changeState(state: T): void {
+    this.component.changeState(state);
+  }
+
+  addObserver(observer: Component<T>): void {
+    this.component.addObserver(observer);
+  }
+
+  removeObserver(observer: Component<T>): void {
+    this.component.removeObserver(observer);
+  }
+
+  notify(state: T): void {
+    this.component.notify(state);
+  }
+
+  addChild(child: Component<T>): void {
+    this.component.addChild(child);
+  }
+
+  removeChild(child: Component<T>): void {
+    this.component.removeChild(child);
+  }
+
+  decorate(): void {
+    console.log('Agregando imagen a la interfaz');
+  }
+}
+
+const newDecorator = new Decorator<string>(
+  new Component<string>('Component decorated')
+);
+const component = new Component<string>('Component');
+
+newDecorator.addObserver(component);
+component.build();
+
+newDecorator.changeState('Nuevo estado');
+
+newDecorator.build();
 
 // #endregion
 
